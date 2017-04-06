64cf00de86dde32bdc92ea8bd231d09515cc4ddf
Yi Fung <yirenfung@gmail.com>
2017-04-06 13:54:49 -0400

    fb7

--- app/components/comment.js
+++ app/components/comment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 import {unixTimeToString} from '../util.js';
 
 export default class Comment extends React.Component {
@@ -9,7 +10,7 @@ export default class Comment extends React.Component {
           PIC
         </div>
         <div className="media-body">
-          <a href="#">{this.props.author.fullName}</a> {this.props.children}
+          <Link to={"/profile/" + this.props.author._id}>{this.props.author.fullName}</Link> {this.props.children}
           <br /><a href="#">Like</a> · <a href="#">Reply</a> ·
             {unixTimeToString(this.props.postDate)}
         </div>
--- app/components/statusupdate.js
+++ app/components/statusupdate.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 import {unixTimeToString} from '../util.js';
 
 export default class StatusUpdate extends React.Component {
@@ -12,7 +13,7 @@ export default class StatusUpdate extends React.Component {
                 PIC
               </div>
               <div className="media-body">
-                <a href="#">{this.props.author.fullName}</a>
+                <Link to={"/profile/" + this.props.author._id}>{this.props.author.fullName}</Link>
                 <br /> {unixTimeToString(this.props.postDate)} · {this.props.location} · <span
                   className="glyphicon glyphicon-user"></span>
               </div>
